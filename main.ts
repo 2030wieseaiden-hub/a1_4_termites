a485d66d3ff1bd131eeef753666037868016b19b
2030wieseaiden-hub <2030wieseaiden@newportgriz.com>
2025-09-24 09:25:58 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+// When the micro:bit isn't shaking, a smiley face appears.
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
